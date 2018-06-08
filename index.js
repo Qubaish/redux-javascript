4273f65f83acc55fd45a2e534e50263ded791d97
Qubaish <qubaish.bhatti@carma.com>
2018-06-09 02:40:00 +0400

    create redux lib code

---
+++ index.js
@@ -0,0 +1,40 @@
+// Lib Code
+
+function createStore(reducer){
+	
+	let state;
+	let listerners = [];
+
+	const getState = () => state;
+
+	const subscribe = (listerner) => {
+		listerners.push(listerner)
+		return () => {
+			listerners = listerners.filter((l) => l !== listerner)
+		}
+	}
+
+	const dispatch = (action) => {
+		let state = reducer(state, action)
+		listerners.forEach((listerner) => listerner())
+	}
+
+	return {
+		getState,
+		subscribe,
+		dispatch
+	} 
+}
+
+// Constants
+
+const ADD_TODO = 'ADD_TODO';
+
+// Reducer
+
+const todos = (state = [], action) => {
+	switch(state.type){
+		case ADD_TODO:
+			return state.concat([action.todo])
+	}
+} 
